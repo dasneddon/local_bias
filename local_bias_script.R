e92b626888a4676179b3d8ac17cc3b954c86c49c
David Sneddon <dasneddon@outlook.com>
2024-10-09 10:44:25 -0400

    modified:   .gitignore modified:   local_bias_script.R modified:   na-map.R modified:   st_abbr.csv modified:   st_pop_center.csv
    modified:   .gitignore
    modified:   local_bias_script.R
    modified:   na-map.R
    modified:   st_abbr.csv
    modified:   st_pop_center.csv

--- local_bias_script.R
+++ local_bias_script.R
@@ -6,7 +6,78 @@ library(utf8)
 library(statcanR)
 library(bea.R)
 library(censusapi)
-source("na-map.R")
+library("ggplot2")
+library("sf")
+library("rnaturalearth")
+library("rnaturalearthdata")
+library("canadianmaps")
+library(plyr)
+library(stringr)
+library(usmap)
+library(spData) 
+library("tools")
+
+gravitymap <- function (x){
+  theme_set(theme_bw())
+  sf_use_s2(FALSE)
+  
+  worldz <- ne_countries(scale = "large", returnclass = "sf")
+  class(worldz)
+  (sites <- data.frame(longitude = c(-172.54, -47.74), latitude = c(23.81,
+                                                                    90)))
+  
+  
+  (sites <- st_as_sf(geodata, coords = c("lon", "lat"),
+                     crs=4369, agr = "constant"))
+  
+  sites <- st_transform(sites, st_crs("ESRI:102010"))
+  states <- st_transform(us_states, st_crs("ESRI:102010"))
+  head(states)
+  provs <- st_sf(PROV[,c(5, 10, 11, 12)])
+  ak <- st_transform(alaska, st_crs("ESRI:102010"))
+  hi <- st_transform(hawaii, st_crs("ESRI:102010"))
+  akhi <- rbind(ak,hi)
+  akhi <- cbind(akhi, st_coordinates(st_centroid(akhi)))
+  akhi <- st_sf(akhi[c(2,7,8,9)])
+  provs <- st_transform(provs, st_crs("ESRI:102010"))
+  
+  
+  states <- cbind(states, st_coordinates(st_centroid(states)))
+  states <- states[c(2, 7, 8, 9)]
+  colnames(provs) <- colnames(akhi) <- colnames(states)
+  
+  head(states)
+
+  
+  head(states)
+  stpr <- st_sf(rbind(as.data.frame(states),
+                      as.data.frame(provs),
+                      as.data.frame(akhi)))
+  
+  tf <- data.frame(us_ca_split[x])
+  prov_abr2 <- prov_abr
+  colnames(prov_abr2) <- colnames(st_abbr)
+  tabrs <- rbind(st_abbr, prov_abr2)
+  colnames(tabrs)[1] <- "NAME"
+  colnames(tf) <- colnames(us_ca)
+  tf$value <- asinh(tf$value)
+  tf <- tf[c(1, 7)]
+  colnames(tf)[1] <- "State"
+  tstpr <- merge(stpr, tabrs, by = "NAME")
+  tstpr <- merge(tstpr, tf, by = "State")
+  
+  ggplot(data = worldz) +
+    geom_sf() +
+    ggtitle(paste("Gravity Map for", prov_abr[prov_abr$from==x,])) +
+    geom_sf(data = tstpr, 
+            aes(fill =value)) +
+    scale_fill_gradient(low="red", high= "green") +
+    coord_sf(xlim = c(-6500000, 3800000), 
+             ylim = c(4700000, -1700000), 
+             expand = FALSE, 
+             crs = st_crs("ESRI:102010"))
+
+}
 #DATA IMPORTS
 url_df <- read.csv("url_df.csv")
 usfips <- read.csv("https://www2.census.gov/geo/docs/reference/state.txt",
@@ -76,9 +147,9 @@ colnames(can_exports)[2] <- "To_GEO"
 can_exports <- merge(can_exports, prov_abr, by="GEO")
 can_exports <- merge(can_exports, provto_abr, by="To_GEO")
 rm(provto_abr)
-can_exports["value"] <- can_exports$VALUE
+can_exports["value"] <- can_exports$VALUE*1000
 can_exports <- can_exports[,-(1:4)]
-can_exports["fttag"] <- paste0(can_exports$from,can_exports$to)
+can_exports["fttag"] <- paste0(can_exports$to,can_exports$from)
 can_exports["intra"] <- -1
 for (i in 1:nrow(can_exports)){
   if(can_exports$from[i]==can_exports$to[i]){
@@ -96,7 +167,6 @@ can_exports["domestic"] <- 0
 
 ###IMPORT AGGREGATE AND CONDENSE CAN_IMPORTS DATAFRAME
 can_imports <- read.csv("ODPFN022_201912N.csv")
-can_imports <- can_imports[complete.cases(can_imports),]
 can_imports <- can_imports[can_imports$Country.Pays=="US",]
 colnames(can_imports) <- c("YearMonth", 
                            "HS2", 
@@ -116,7 +186,7 @@ for (i in 1:nrow(can_imports)){
 }
 can_imports <- can_imports[,c(2, 3, 4)]
 colnames(can_imports) <- c("to", "from", "value")
-can_imports["fttag"] <- paste0(can_imports$from,can_imports$to)
+can_imports["fttag"] <- paste0(can_imports$to,can_imports$from)
 can_imports["domestic"] <- 1
 
 ###CANADIAN GDP
@@ -171,4 +241,6 @@ reg2 <- lm(asinh(value) ~ asinh(from_gdp) +
              domestic, data=us_ca)
 summary(reg2)
 
-
+for(i in unique(prov_abr$from)){
+  ggsave(paste0(i,".pdf"),gravitymap(i))
+}
--- na-map.R
+++ na-map.R
@@ -4,59 +4,70 @@ library("sf")
 library("rnaturalearth")
 library("rnaturalearthdata")
 library("canadianmaps")
-source("local_bias_script.R")
 library(plyr)
 library(stringr)
+library(usmap)
+library(spData) 
 
-world <- ne_countries(scale = "large", returnclass = "sf")
-class(world)
+gravitymap <- function (x){
+sf_use_s2(FALSE)
+
+worldz <- ne_countries(scale = "large", returnclass = "sf")
+class(worldz)
 (sites <- data.frame(longitude = c(-172.54, -47.74), latitude = c(23.81,
                                                                   90)))
 
 
 (sites <- st_as_sf(geodata, coords = c("lon", "lat"),
-                   crs = 4369, agr = "constant"))
+                   crs=4369, agr = "constant"))
 
-
+sites <- st_transform(sites, st_crs("ESRI:102010"))
 library("maps")
-states <- st_as_sf(map("state", plot = FALSE, fill = TRUE))
+states <- st_transform(us_states, st_crs("ESRI:102010"))
 head(states)
-provs <- PROV[,c(5, 10, 11, 12)]
+provs <- st_sf(PROV[,c(5, 10, 11, 12)])
+ak <- st_transform(alaska, st_crs("ESRI:102010"))
+hi <- st_transform(hawaii, st_crs("ESRI:102010"))
+akhi <- rbind(ak,hi)
+akhi <- cbind(akhi, st_coordinates(st_centroid(akhi)))
+akhi <- st_sf(akhi[c(2,7,8,9)])
+provs <- st_transform(provs, st_crs("ESRI:102010"))
 
 
 states <- cbind(states, st_coordinates(st_centroid(states)))
-colnames(provs) <- colnames(states)
+states <- states[c(2, 7, 8, 9)]
+colnames(provs) <- colnames(akhi) <- colnames(states)
 
 head(states)
 library("tools")
-states$ID <- toTitleCase(states$ID)
+
 head(states)
-stpr <- st_sf(rbind(as.data.frame(states),as.data.frame(provs)))
-gravitymap <- function (x){
+stpr <- st_sf(rbind(as.data.frame(states),
+                    as.data.frame(provs),
+                    as.data.frame(akhi)))
+
   tf <- data.frame(us_ca_split[x])
   prov_abr2 <- prov_abr
   colnames(prov_abr2) <- colnames(st_abbr)
   tabrs <- rbind(st_abbr, prov_abr2)
-  colnames(tabrs)[1] <- "ID"
+  colnames(tabrs)[1] <- "NAME"
   colnames(tf) <- colnames(us_ca)
+  tf$value <- asinh(tf$value)
   tf <- tf[c(1, 7)]
-  colnames(tf)[1] <- "state"
-  tstpr <- merge(stpr, tabrs, by = "ID")
+  colnames(tf)[1] <- "State"
+  tstpr <- merge(stpr, tabrs, by = "NAME")
   tstpr <- merge(tstpr, tf, by = "State")
 
-  ggplot(data = world) +
+  ggplot(data = worldz) +
     geom_sf() +
     geom_sf(data = tstpr, 
             aes(fill =value)) +
     scale_fill_gradient(low="red", high= "green") +
-    geom_sf(data = sites,  
-            size = 1,
-            shape = 23, 
-            fill = "darkred") +
     coord_sf(xlim = c(-6500000, 3800000), 
              ylim = c(4700000, -1700000), 
              expand = FALSE, 
              crs = st_crs("ESRI:102010"))
+  return {NULL}
 }
 ###https://r-spatial.org/r/2018/10/25/ggplot2-sf-2.html###
 
